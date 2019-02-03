40f3d4741b9f4fdba5debb55422e62c48fb3be00
Crul <rauljavier.vila@gmail.com>
2019-02-03 08:44:48 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Repositorio de fórmulas para el [Curso de Teoría Cuántica de Campos](https://www.youtube.com/playlist?list=PLAnA8FVrBl8BiQd_Fg-Jr32P_v9F8vG-2) de [Javier García](https://www.youtube.com/user/jamesjamesbondbond)
+
+Sitio web: [https://crul.github.io/CursoTeoriaDeCamposJavierGarcia](https://crul.github.io/CursoTeoriaDeCamposJavierGarcia)
---
+++ script.js
@@ -0,0 +1,128 @@
+// https://github.com/Khan/KaTeX
+var katexOptions = { displayMode: true };
+var closeSymbol = '&#128449;';
+var openSymbol = '&#128448;';
+var gotoTopSymbol = '&uarr;';
+var toggleFormulasCssClass = 'toggle-formulas-btn';
+var titleRightBtnsCssClass = 'title-right-btns';
+
+var formulasElems = $('.formulas').hide();
+
+var gotoTopBtn = $('<a>')
+  .attr('href', '#top')
+  .html(gotoTopSymbol + ' Menú')
+  .addClass('goto-top-btn');
+var gotoTopBtnTmpl = $('<div>')
+  .addClass(titleRightBtnsCssClass)
+  .append(gotoTopBtn);
+
+var toggleFormulasBtnElems;
+var toggleFormulasBtnTmpl = $('<span>')
+  .html(openSymbol)
+  .addClass(toggleFormulasCssClass);
+
+$(document).ready(bootstrap);
+
+function bootstrap() {
+  $('.formula').each(renderFormula);
+
+  toggleFormulasBtnElems = $('h2')
+    .each(addGotoTopBtn)
+    .map(addToggleBtn);
+
+  $('#menu li')
+    .map(getMenuLinks)
+    .each(addUnfoldEvent)
+    .each(addVideoLink);
+
+  $('#foldAllBtn').click(foldAll);
+  $('#unfoldAllBtn').click(unfoldAll);
+  $('.formulas-title').mousedown(unfoldTarget);
+  $('.formula-link').mousedown(unfoldTarget);
+
+  $('.spoiler-btn').click(showSpoiler);
+
+  $('html, body').css('min-height', 0);
+  
+  handleInitialSection();
+}
+
+function handleInitialSection() {
+  var hash = location.hash;
+  location.hash = '';
+  location.hash = hash;
+
+  if (hash && hash != 'top') {
+    $(hash.replace('#', '.')).slideToggle();
+    $(hash + ' .' + toggleFormulasCssClass).html(closeSymbol);
+  }
+}
+
+function renderFormula(i, elem) {
+  katex.render(elem.innerText, elem, katexOptions);
+}
+
+function addGotoTopBtn(index, h2Elem) {
+  $(h2Elem).prepend(gotoTopBtnTmpl.clone());
+}
+
+function addToggleBtn(index, h2Elem) {
+   return toggleFormulasBtnTmpl
+    .clone()
+    .click(toggleFolding)
+    .prependTo(h2Elem)[0];
+}
+
+function getMenuLinks(index, menuOptElem) {
+  var menuLinks = $(menuOptElem).find('a');
+
+  return {
+    formulasLink: menuLinks[0],
+    videoLink: menuLinks[1]
+  };
+}
+
+function addUnfoldEvent(index, menuLinks) {
+  $(menuLinks.formulasLink).mousedown(unfoldTarget);
+}
+
+function addVideoLink(index, menuLinks) {
+  var formulasId = getIdFromHref(menuLinks.formulasLink);
+  var videoLink = $(menuLinks.videoLink).clone().addClass('view-video-btn');
+  $('#' + formulasId + ' .' + titleRightBtnsCssClass).append(videoLink);
+}
+
+function foldAll() {
+  formulasElems.slideUp();
+  toggleFormulasBtnElems.html(openSymbol);
+}
+
+function unfoldAll() {
+  formulasElems.slideDown();
+  toggleFormulasBtnElems.html(closeSymbol);
+}
+
+function unfoldTarget(ev) {
+  var formulasId = getIdFromHref(ev.currentTarget);
+  var formulasElem = $('.' + formulasId);
+  if (!formulasElem.is(':visible')) {
+    formulasElem.show();
+    $('#' + formulasId + ' .' + toggleFormulasCssClass).html(closeSymbol);
+  }
+}
+
+function toggleFolding(ev) {
+  var formulasElem = $('.' + ev.currentTarget.parentElement.id);
+  var isHiding = formulasElem.is(':visible');
+  formulasElem.slideToggle();
+  $(ev.currentTarget).html(isHiding ? openSymbol : closeSymbol);
+}
+
+function showSpoiler(ev) {
+  $(ev.currentTarget).hide();
+  $(ev.currentTarget.parentElement).find('.spoiler').fadeIn();
+}
+
+function getIdFromHref(elem) {
+  return elem.href.match(/#(.*)/)[1];
+}
